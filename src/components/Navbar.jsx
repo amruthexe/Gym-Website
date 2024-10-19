e8029a7db0548a271a12e64dfbf258f824d358e4
amruthexe <159619983+amruthexe@users.noreply.github.com>
2024-10-19 14:24:29 +0530

    Updates Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -50,14 +50,11 @@ const Navbar = () => {
               ))}
             </ul>
             <div className="flex space-x-6">
-              <a href="#" className="py-2 px-3 border rounded-md">
-                Sign In
-              </a>
               <a
                 href="#"
                 className="py-2 px-3 rounded-md bg-gradient-to-r from-orange-500 to-orange-800"
               >
-              
+              Bokk
               </a>
             </div>
           </div>
